575b88c501653716d23546c5f649d9ac4b48da80
MoeyJac <jo3ymac@gmail.com>
2023-06-22 13:25:49 -0400

    feat:  added verification of contract source

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,27 +1,38 @@
 const { network } = require("hardhat")
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const { chainId } = network.config.chainId
+    const chainId = await getChainId()
 
     let ethUsdPriceFeedAddress
 
+    // when going for localhost or hardhat we can use a mock
     if (developmentChains.includes(network.name)) {
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
+        console.log(`Chain Id: ${chainId}`)
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
 
-    // when going for localhost or hardhat we can use a mock
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress], // include price feed address
+        args: args, // include price feed address
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
     log("---------------------------------------------")
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
 }
 
 module.exports.tags = ["all", "fundme"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,7 @@ const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+const REPORT_GAS = process.env.REPORT_GAS !== undefined
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -20,6 +21,7 @@ module.exports = {
             url: SEPOLIA_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 11155111,
+            blockConfirmations: 6,
         },
         localhost: {
             url: "http://127.0.0.1:8545/",
@@ -33,7 +35,7 @@ module.exports = {
         apiKey: ETHERSCAN_API_KEY,
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
+        enabled: REPORT_GAS,
         outputFile: "gas-report.txt",
         noColors: true,
         currency: "USD",
