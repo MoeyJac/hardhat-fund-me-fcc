f9dec8963c8c894094b1829023a825d2381cdc02
MoeyJac <jo3ymac@gmail.com>
2023-06-22 16:19:12 -0400

    feat: updated formatting and documentation

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,8 +6,14 @@ import "./PriceConverter.sol";
 
 //constant, immutable used for gas savings
 
-error NotOwner();
+error FundMe__NotOwner();
 
+/**
+ * @title A contract for crowd functing
+ * @author Moeyjac
+ * @notice This contract is to demo a sample funding contract
+ * @dev This implements price feeds as our library
+ */
 contract FundMe {
     using PriceConverter for uint256;
 
@@ -19,18 +25,36 @@ contract FundMe {
     address public immutable i_owner;
 
     AggregatorV3Interface public priceFeed;
+
+    modifier onlyOwner(){
+        // require(i_owner == msg.sender, "Sender is not the owner.");
+        if(msg.sender != i_owner){ revert FundMe__NotOwner(); }
+        _;
+    }
     
     constructor(address _priceFeed){
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(_priceFeed);
     }
 
-    modifier onlyOwner(){
-        // require(i_owner == msg.sender, "Sender is not the owner.");
-        if(msg.sender != i_owner){ revert NotOwner(); }
-        _;
+    // What happens if someone sens this contract ETH without calling the fund function?
+
+    // Used if msg.data is blank
+    receive() external payable {
+        // Call fund function if value was sent with no specific function called
+        fund();
     }
 
+    // Used if msg.data is not empty OR msg.data is empty and no receive function is implemented
+    fallback() external payable {
+        // Call fund function if value was sent with no specific function called
+        fund();
+    }
+
+    /**
+     * @notice This function funds this contract
+     * @dev This impleements price feeds as our library
+     */
     function fund() public payable {
         // Want to be able to specify a minimum amount for deposit
          require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "Didnt meet minimum deposit amount");
@@ -38,6 +62,10 @@ contract FundMe {
          addressToAmountFunded[msg.sender] += msg.value;
     }
 
+    /**
+     * @notice This function allows the owner to withraw the balance of 
+     * the contract
+     */
     function withdraw() public onlyOwner() {
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
@@ -62,20 +90,6 @@ contract FundMe {
         // most used function to transfer ethereum 
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
-    }
-
-    // What happens if someone sens this contract ETH without calling the fund function?
-
-    // Used if msg.data is blank
-    receive() external payable {
-        // Call fund function if value was sent with no specific function called
-        fund();
-    }
-
-    // Used if msg.data is not empty OR msg.data is empty and no receive function is implemented
-    fallback() external payable {
-        // Call fund function if value was sent with no specific function called
-        fund();
     }
 
 }
